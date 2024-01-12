c16237ec928ecbf728870a6fc8b4936c90c34b24
FaizanDurrani <faizandurrani324@gmail.com>
2024-01-12 18:09:53 +0000

    Deploying to gh-pages from @ Paperback-iOS/extensions-default@2c4913c3393ba49f9461d08e34f00984a1d8c9b1 🚀

--- 0.8/Paperback/source.js
+++ 0.8/Paperback/source.js
@@ -3255,7 +3255,7 @@ const testServerSettings = (stateManager, requestManager) => __awaiter(void 0, v
     // To test these information, we try to make a connection to the server
     // We could use a better endpoint to test the connection
     const request = App.createRequest({
-        url: `${komgaAPI}/libraries/`,
+        url: `${komgaAPI}/libraries`,
         method: 'GET',
         headers: { authorization: authorization }
     });
