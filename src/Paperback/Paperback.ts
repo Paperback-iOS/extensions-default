0c4df8635b757350893a0898a178c9f853263324
Faizan Durrani <faizandurrani324@gmail.com>
2024-01-12 23:09:37 +0500

    Update Paperback.ts

--- src/Paperback/Paperback.ts
+++ src/Paperback/Paperback.ts
@@ -39,7 +39,7 @@ import { getAuthorizationString,
 //  - getTags() which is called on the homepage
 //  - search method which is called even if the user search in an other source
 export const PaperbackInfo: SourceInfo = {
-    version: '1.2.10',
+    version: '1.2.11',
     name: 'Paperback',
     icon: 'icon.png',
     author: 'Lemon | Faizan Durrani',
