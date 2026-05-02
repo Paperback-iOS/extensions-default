26a79cfb5a40de75218a4fccd0d05433e2ea58d8
Exikle <Exikle@users.noreply.github.com>
2026-05-01 23:58:55 -0400

    fix(Komga): use publishDate field for chapter release date

--- src/Komga/komga.ts
+++ src/Komga/komga.ts
@@ -379,7 +379,7 @@ export class KomgaExtension implements IKomgaExtension {
         volume: 0,
         langCode: book.size,
         title: (book.metadata.title ?? '').replace(/^chapter\s+[\d.]+[:\s-]*/i, '').trim(),
-        creationDate: book.metadata.releaseDate ? new Date(book.metadata.releaseDate) : new Date(book.fileLastModified),
+        publishDate: book.metadata.releaseDate ? new Date(book.metadata.releaseDate) : new Date(book.fileLastModified),
         sortingIndex: book.metadata.numberSort,
         sourceManga: sourceManga,
         version: languageCode,
